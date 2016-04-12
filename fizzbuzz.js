c488c3d082acc224fc8676f32bd99f9039a482da
Raymond Yeh <ryeh88@gmail.com>
2016-04-11 21:04:51 -0700

    new js 4.11

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,7 +1,7 @@
-function fizz () {
+function fizzBuzz () {
 	for(var i = 1; i <= 100; i++) {
 		var x = i;
-		if (i % 15 == 0)
+		if (i % 5 == 0 && i % 3 == 0)
 			x = "FizzBuzz"
 		else if (i % 3 == 0) 
 			x = "Fizz"
